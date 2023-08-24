0f64c4ae1e7079c5ce3dd37a4bceb451a0da775c
Robert J. <chew-z@users.noreply.github.com>
2023-08-24 20:45:13 +0200

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module bradio
+
+go 1.21.0
+
+require gitlab.com/AgentNemo/goradios v0.0.0-20210218201446-d20f1eeff610
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"gitlab.com/AgentNemo/goradios"
+)
+
+func main() {
+	var stations []goradios.Station
+	limit := 12
+	if os.Args[3] == "--limit" {
+		limit, _ = strconv.Atoi(os.Args[4])
+	}
+	if os.Args[1] == "--name" {
+		stations = goradios.FetchStationsDetailed(goradios.StationsByName, os.Args[2], goradios.StationsOrderClickCount, true, uint(0), uint(limit), true)
+	} else if os.Args[1] == "--tag" {
+		stations = goradios.FetchStationsDetailed(goradios.StationsByTagExact, os.Args[2], goradios.StationsOrderClickCount, true, uint(0), uint(limit), true)
+	} else {
+		stations = goradios.FetchStations(goradios.StationsByName, os.Args[1])
+	}
+	for _, station := range stations {
+		s := fmt.Sprintf("(%d) %s; %s; %s[%d]; %s", station.ClickCount, station.Name, station.Tags, station.Codec, station.Bitrate, station.URL)
+		fmt.Println(s)
+	}
+}
