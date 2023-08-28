b39e52eeb6da7639d7d79fdc9edbb5fbb49757ec
Robert J. <chew-z@users.noreply.github.com>
2023-08-28 21:09:11 +0200

    Additional check for --limit argument

--- main.go
+++ main.go
@@ -11,13 +11,13 @@ import (
 func main() {
 	var stations []goradios.Station
 	limit := 12
-	if os.Args[3] == "--limit" {
+	if len(os.Args) > 4 && os.Args[3] == "--limit" {
 		limit, _ = strconv.Atoi(os.Args[4])
 	}
 	if os.Args[1] == "--name" {
 		stations = goradios.FetchStationsDetailed(goradios.StationsByName, os.Args[2], goradios.StationsOrderClickCount, true, uint(0), uint(limit), true)
 	} else if os.Args[1] == "--tag" {
-		stations = goradios.FetchStationsDetailed(goradios.StationsByTagExact, os.Args[2], goradios.StationsOrderClickCount, true, uint(0), uint(limit), true)
+		stations = goradios.FetchStationsDetailed(goradios.StationsByTagExact, os.Args[2], goradios.StationsOrderClickTrend, true, uint(0), uint(limit), true)
 	} else {
 		stations = goradios.FetchStations(goradios.StationsByName, os.Args[1])
 	}
